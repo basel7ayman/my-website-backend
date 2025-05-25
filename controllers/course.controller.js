3b29a492f2f3d9cd4d407d69bb81b8fde3b381c2
basel7ayman <basel.elsa3di@gmail.com>
2025-05-26 00:51:17 +0300

    Remove unused user enrollment functionality from course controller

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,7 +1,6 @@
 import { Course } from "../models/course.model.js";
 import { Lecture } from "../models/lecture.model.js";
 import {deleteMediaFromCloudinary, deleteVideoFromCloudinary, uploadMedia} from "../utils/cloudinary.js";
-import { User } from "../models/user.model.js";
 
 export const createCourse = async (req,res) => {
     try {
@@ -337,10 +336,3 @@ export const togglePublishCourse = async (req,res) => {
         })
     }
 }
-
-export const enrollInCourse = async (req, res) => {
-  const { userId, courseId } = req.body;
-  await User.findByIdAndUpdate(userId, { $addToSet: { enrolledCourses: courseId } });
-  await Course.findByIdAndUpdate(courseId, { $addToSet: { enrolledStudents: userId } });
-  res.json({ success: true });
-};
