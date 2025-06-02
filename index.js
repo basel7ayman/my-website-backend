3d7a309a91074ba360f1550815ca0cbfb31fd32b
basel7ayman <basel.elsa3di@gmail.com>
2025-06-03 02:36:10 +0300

    Update CORS configuration to reflect the correct frontend URL for improved access.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const allowedOrigins = [
-    'https://basel7ayman.github.io/my-website-frontend/',
+    'https://basel7ayman.github.io/',
     'http://localhost:5173'
 ];
 
